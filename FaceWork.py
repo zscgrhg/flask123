fd31f25761b479850aa56ba9eacfb1d80e7d283b
zscgrhg <zscgrhg@qq.com>
2018-11-25 20:44:47 +0800

    iii

--- FaceWork.py
+++ FaceWork.py
@@ -24,6 +24,7 @@ images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
 embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
 phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
 
+pnet, rnet, onet = align.detect_face.create_mtcnn(sess, None)
 
 def main(args):
     images = load_and_align_data(args.image_files, args.image_size, args.margin, args.gpu_memory_fraction)
@@ -48,12 +49,16 @@ def main(args):
     for i in range(nrof_images):
         print('    %1d     ' % i, end='')
     print('')
+
+    retMatrix = []
     for i in range(nrof_images):
         print('%1d  ' % i, end='')
         for j in range(nrof_images):
             dist = np.sqrt(np.sum(np.square(np.subtract(emb[i, :], emb[j, :]))))
+            retMatrix.append((i, j, dist))
             print('  %1.4f  ' % dist, end='')
         print('')
+    return retMatrix
 
 
 def load_and_align_data(image_paths, image_size, margin, gpu_memory_fraction):
@@ -62,11 +67,7 @@ def load_and_align_data(image_paths, image_size, margin, gpu_memory_fraction):
     factor = 0.709  # scale factor
 
     print('Creating networks and loading parameters')
-    with tf.Graph().as_default():
-        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_memory_fraction)
-        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
-        with sess.as_default():
-            pnet, rnet, onet = align.detect_face.create_mtcnn(sess, None)
+
 
     tmp_image_paths = copy.copy(image_paths)
     img_list = []
@@ -86,6 +87,7 @@ def load_and_align_data(image_paths, image_size, margin, gpu_memory_fraction):
         bb[3] = np.minimum(det[3] + margin / 2, img_size[0])
         cropped = img[bb[1]:bb[3], bb[0]:bb[2], :]
         aligned = misc.imresize(cropped, (image_size, image_size), interp='bilinear')
+        # misc.imsave("oops", aligned)
         prewhitened = facenet.prewhiten(aligned)
         img_list.append(prewhitened)
     images = np.stack(img_list)
@@ -107,7 +109,30 @@ def parse_arguments(argv):
     return parser.parse_args(argv)
 
 
+def save(image, simpleName, image_size=160, margin=44, gpu_memory_fraction=1.0):
+    minsize = 20  # minimum size of face
+    threshold = [0.6, 0.7, 0.7]  # three steps's threshold
+    factor = 0.709  # scale factor
+
+    print('Creating networks and loading parameters')
+
+    img = misc.imread(os.path.expanduser(image), mode='RGB')
+    img_size = np.asarray(img.shape)[0:2]
+    bounding_boxes, _ = align.detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+    if len(bounding_boxes) < 1:
+        print("can't detect face from ", image)
+        return
+    det = np.squeeze(bounding_boxes[0, 0:4])
+    bb = np.zeros(4, dtype=np.int32)
+    bb[0] = np.maximum(det[0] - margin / 2, 0)
+    bb[1] = np.maximum(det[1] - margin / 2, 0)
+    bb[2] = np.minimum(det[2] + margin / 2, img_size[1])
+    bb[3] = np.minimum(det[3] + margin / 2, img_size[0])
+    cropped = img[bb[1]:bb[3], bb[0]:bb[2], :]
+    aligned = misc.imresize(cropped, (image_size, image_size), interp='bilinear')
+    misc.imsave("static/facedb/" + simpleName, aligned)
+
 def compare(face1, face2):
-    main(parse_arguments(["model/20180408-102900.pb", face1, face2]))
+    return main(parse_arguments(["model/20180408-102900.pb", face1, face2]))
 
 # main(parse_arguments(["model/20180408-102900.pb","face/hx1.jpg","face/hx2.jpg"]))
--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@
 # 2018/11/25
 
 from flask import Flask, redirect, url_for
+from werkzeug.utils import secure_filename
 
 import FaceWork as fw
 import FaceWork2 as fw2
@@ -16,8 +17,8 @@ import time
 
 
 @app.route('/index')
-def hello():
-    return render_template('hello.html')
+def hello(matrix=[]):
+    return render_template('hello.html', matrix=matrix)
 
 
 @app.route('/index2')
@@ -29,25 +30,42 @@ def hello2():
 def upload_file():
     if request.method == 'POST':
         start = time.perf_counter()
-        f = request.files['face1']
-        f.save('face/face1.png')
-        f = request.files['face2']
-        f.save('face/face2.png')
-        fw.compare("face/face1.png", "face/face2.png")
+        f1 = request.files['face1']
+        name1 = 'static/face/' + secure_filename(f1.filename)
+        f1.save(name1)
+        f2 = request.files['face2']
+        name2 = 'static/face/' + secure_filename(f2.filename)
+        f2.save(name2)
+        retmatrix = fw.compare(name1, name2)
         end = time.perf_counter()
         print('fw1 executed in %s ms' % ((end - start) * 1000))
-    return redirect(url_for('hello'))
+        print(retmatrix)
+        return render_template('hello.html', matrix=retmatrix)
 
+
+@app.route('/save', methods=['POST'])
+def save():
+    if request.method == 'POST':
+        start = time.perf_counter()
+        face = request.files['face']
+        simpleName = secure_filename(face.filename)
+        fileName = 'static/face/' + simpleName
+        face.save(fileName)
+        fw.save(fileName, simpleName)
+        end = time.perf_counter()
+        print('save executed in %s ms' % ((end - start) * 1000))
+
+        return render_template('hello.html')
 
 @app.route('/upload2', methods=['POST'])
 def upload_file2():
     if request.method == 'POST':
         start = time.perf_counter()
         f = request.files['face1']
-        f.save('face/face1.png')
+        f.save('static/face/face1.png')
         f = request.files['face2']
-        f.save('face/face2.png')
-        fw2.compare("face/face1.png", "face/face2.png")
+        f.save('static/face/face2.png')
+        fw2.compare("static/face/face1.png", "static/face/face2.png")
         end = time.perf_counter()
         print('fw2 executed in %s ms' % ((end - start) * 1000))
     return redirect(url_for('hello2'))
